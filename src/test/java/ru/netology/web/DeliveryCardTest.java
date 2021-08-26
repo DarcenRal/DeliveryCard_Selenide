d1131913e4466c201795d88ad2e032f4caf1bdfe
DarcenRal <75911924+DarcenRal@users.noreply.github.com>
2021-08-26 20:25:58 +0300

    Add test

---
+++ src/test/java/ru/netology/web/DeliveryCardTest.java
@@ -0,0 +1,38 @@
+package ru.netology.web;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.withText;
+
+
+public class DeliveryCardTest {
+    LocalDate today = LocalDate.now();
+    LocalDate later = today.plusDays(3);
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    @BeforeEach
+    void setUp() {
+        open("http://localhost:9999/");
+    }
+
+
+    @Test
+    void shouldEnteringValidValues() {
+        open("http://localhost:9999");
+        $("[data-test-id='city'] input").setValue("Санкт-Петербург");
+        $("[data-test-id='date'] input").setValue("formatter.format(later)");
+        $("[data-test-id='name'] input").setValue("Кононов Яков");
+        $("[data-test-id='phone'] input").setValue("+79002233222");
+        $("[data-test-id='agreement']").click();
+        $(By.className("button")).click();
+        $(withText("Успешно!")).shouldBe(visible, Duration.ofSeconds(15));
+    }
+}
