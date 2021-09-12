93ddcbdffa1ce583e212635469d3bf919100849c
DarcenRal <75911924+DarcenRal@users.noreply.github.com>
2021-09-12 14:10:26 +0300

    change test

--- src/test/java/ru/netology/web/DeliveryCardTest.java
+++ src/test/java/ru/netology/web/DeliveryCardTest.java
@@ -17,12 +17,8 @@ import static com.codeborne.selenide.Selectors.withText;
 
 
 public class DeliveryCardTest {
-    LocalDate today = LocalDate.now();
-    LocalDate later = today.plusDays(3);
-    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-    @BeforeEach
-    void setUp() {
-        open("http://localhost:9999/");
+    String meetingDay(int day){
+        return LocalDate.now().plusDays(day).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
     }
 
 
@@ -30,13 +26,14 @@ public class DeliveryCardTest {
     void shouldEnteringValidValues() {
         open("http://localhost:9999");
         $("[data-test-id='city'] input").setValue("Санкт-Петербург");
-        $("[data-test-id='date'] input").setValue("formatter.format(later)");
+        $("[data-test-id='date'] input").doubleClick().sendKeys(Keys.BACK_SPACE);
+        $("[data-test-id='date'] input").setValue(meetingDay(5));
         $("[data-test-id='name'] input").setValue("Кононов Яков");
         $("[data-test-id='phone'] input").setValue("+79002233222");
         $("[data-test-id='agreement']").click();
         $(By.className("button")).click();
         $(withText("Успешно!")).shouldBe(visible, Duration.ofSeconds(15));
         $("[data-test-id='notification'] .notification__content")
-                .shouldHave(exactText("Встреча успешно забронирована на " + formatter.format(later)));
+                .shouldHave(exactText("Встреча успешно забронирована на " + meetingDay(5)));
     }
 }
